1756d82cf0b1566703a0d9d6e60534d5618501ce
dani <mayor@dani.town>
2019-11-02 18:50:57 -0400

    init

---
+++ index.js
@@ -0,0 +1,71 @@
+/*
+*
+*  Runs every 24 hours
+*  Archives the daily CO2 ppm (from NOAA ESRL) to the Arweave network
+*  https://www.arweave.org/
+*
+*/
+
+const fetch = require('node-fetch')
+const Arweave = require('arweave/node');
+const key = require('./arweave-wallet.json')
+const Client = require('ftp');
+const fs = require('fs');
+
+
+// init Arweave
+const arweave = Arweave.init({
+  host: 'arweave.net',
+  port: 443,
+  protocol: 'https'
+});
+
+void async function main() {
+  // get the file from NOAA
+  await fetchFromFTP()
+
+  // push it to the arweave network
+  pushToArweave()
+
+}()
+
+async function fetchFromFTP() {
+  let ftp = new Client();
+  ftp.on('ready', function() {
+    ftp.get('/products/trends/co2/co2_trend_gl.txt', function(err, stream) {
+      if (err) throw err;
+      stream.once('close', function() { ftp.end(); });
+      stream.pipe(fs.createWriteStream('co2_ppm.txt'));
+    });
+  });
+  ftp.connect({
+    host: "aftp.cmdl.noaa.gov",
+    port: 21
+  });
+}
+
+async function pushToArweave() {
+  // read file from fs
+  let  file = fs.readFileSync('co2_ppm.txt', 'utf8');
+
+  // create transaction
+  let transaction = await arweave.createTransaction({
+    data: Buffer.from(file, 'utf8')
+  }, key);
+
+
+  // tag the transaction so it can be found later
+  // TODO: later change this to something else to discern test from prod
+  transaction.addTag('dani_project_test', 'dailyco2ppm_test')
+
+  // sign the transaction
+  await arweave.transactions.sign(transaction, key);
+
+  // then send it to arweave
+  const response = await arweave.transactions.post(transaction);
+
+  // return transaction id. data will be hosted at arweave.net/transaction-id
+  let transactionId = JSON.parse(response.config.data)["id"]
+  console.log(response.status, response.data, `https://arweave.net/${transactionId}`);
+  return transactionId
+}
